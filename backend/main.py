89b469f47b36146669f5fc1de3965797aa506775
olashin <omarlashin29@gmail.com>
2026-01-11 03:51:55 -0800

    fixed conflicts

--- backend/main.py
+++ backend/main.py
@@ -39,6 +39,8 @@ app = FastAPI(lifespan=lifespan)
 origins = [
     "http://localhost:5175",
     "http://127.0.0.1:5175",
+    "http://localhost:5174",
+    "http://127.0.0.1:5174",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
 ]
